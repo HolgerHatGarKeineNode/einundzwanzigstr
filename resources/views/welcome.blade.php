50f7f77c697761b4e3e974395794455181eb1c84
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-08-29 23:59:20 +0200

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @googlefonts
 
         <!-- Styles -->
         <style>

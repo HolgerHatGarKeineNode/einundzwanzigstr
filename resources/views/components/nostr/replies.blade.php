c314ce97fc9ab7ff3d1501e73323490214c1c4e6
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-08-30 21:46:24 +0200

    updates

--- resources/views/components/nostr/replies.blade.php
+++ resources/views/components/nostr/replies.blade.php
@@ -1,20 +1,22 @@
-<template x-if="eventsReplies[event.id]">
-    <div class="flow-root p-2 pb-12">
-        <ul role="list" class="-mb-8">
-            <template x-for="(reply, index) in eventsReplies[event.id]" :key="reply.id">
-                <li class="outline outline-amber-500/5 outline-offset-1">
-                    <div class="relative pb-8">
+<div>
+    <h3 class="pb-2">Replies</h3>
+    <template x-if="eventsReplies[event.id]">
+        <div class="flow-root p-2 pb-12">
+            <ul role="list" class="-mb-8">
+                <template x-for="(reply, index) in eventsReplies[event.id]" :key="reply.id">
+                    <li class="outline outline-amber-500/5 outline-offset-1">
+                        <div class="relative pb-8">
                         <span x-show="eventsReplies[reply.id]"
                               class="absolute left-5 top-5 -ml-px h-full w-0.5 bg-amber-500" aria-hidden="true"></span>
-                        <div class="relative flex items-start space-x-3">
-                            <div class="relative">
-                                <template x-if="authorMetaData[reply.pubkey]">
-                                    <img class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-amber-500"
-                                         :src="authorMetaData[reply.pubkey].image"
-                                         :alt="authorMetaData[reply.pubkey].display_name && authorMetaData[reply.pubkey].display_name[0]"
-                                    >
-                                </template>
-                                <span class="absolute -bottom-0.5 -right-1 rounded-tl bg-transparent px-0.5 py-px">
+                            <div class="relative flex items-start space-x-3">
+                                <div class="relative">
+                                    <template x-if="authorMetaData[reply.pubkey]">
+                                        <img class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-amber-500"
+                                             :src="authorMetaData[reply.pubkey].image"
+                                             :alt="authorMetaData[reply.pubkey].display_name && authorMetaData[reply.pubkey].display_name[0]"
+                                        >
+                                    </template>
+                                    <span class="absolute -bottom-0.5 -right-1 rounded-tl bg-transparent px-0.5 py-px">
                                   <svg class="h-5 w-5 text-purple-500" viewBox="0 0 20 20" fill="currentColor"
                                        aria-hidden="true">
                                     <path fill-rule="evenodd"
@@ -22,42 +24,42 @@
                                           clip-rule="evenodd"/>
                                   </svg>
                                 </span>
-                            </div>
-                            <div class="min-w-0 flex-1">
-                                <template x-if="authorMetaData[reply.pubkey]">
-                                    <div class="flex justify-between px-2 pt-2">
-                                        <div class="text-sm">
-                                            <div class="font-bold text-gray-200"
-                                                 x-text="authorMetaData[reply.pubkey].display_name"></div>
+                                </div>
+                                <div class="min-w-0 flex-1">
+                                    <template x-if="authorMetaData[reply.pubkey]">
+                                        <div class="flex justify-between px-2 pt-2">
+                                            <div class="text-sm">
+                                                <div class="font-bold text-gray-200"
+                                                     x-text="authorMetaData[reply.pubkey].display_name"></div>
+                                            </div>
+                                            <p class="mt-0.5 text-sm text-gray-300 italic"
+                                               x-text="formatDate(reply.created_at)"></p>
                                         </div>
-                                        <p class="mt-0.5 text-sm text-gray-300 italic"
-                                           x-text="formatDate(reply.created_at)"></p>
+                                    </template>
+                                    <div class="mt-2 text-sm text-gray-200">
+                                        <p x-html="parseContent(reply)"></p>
                                     </div>
-                                </template>
-                                <div class="mt-2 text-sm text-gray-200">
-                                    <p x-html="parseContent(reply)"></p>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <template x-if="eventsReplies[reply.id]">
-                        <ul role="list" class="ml-6 mb-3">
-                            <template x-for="(r, i) in eventsReplies[reply.id]">
-                                <li>
-                                    <div class="relative pb-8">
+                        <template x-if="eventsReplies[reply.id]">
+                            <ul role="list" class="ml-6 mb-3">
+                                <template x-for="(r, i) in eventsReplies[reply.id]">
+                                    <li>
+                                        <div class="relative pb-8">
                                         <span
                                                 x-show="i + 1 !== eventsReplies[reply.id].length || eventsReplies[r.id]"
                                                 class="absolute left-5 top-5 rotate-180 -ml-px h-full w-0.5 bg-amber-500"
                                                 aria-hidden="true"></span>
-                                        <div class="relative flex items-start space-x-3">
-                                            <div class="relative">
-                                                <template x-if="authorMetaData[r.pubkey]">
-                                                    <img class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-amber-500"
-                                                         :src="authorMetaData[r.pubkey].image"
-                                                         :alt="authorMetaData[r.pubkey].display_name && authorMetaData[r.pubkey].display_name[0]"
-                                                    >
-                                                </template>
-                                                <span class="absolute -bottom-0.5 -right-1 rounded-tl bg-transparent px-0.5 py-px">
+                                            <div class="relative flex items-start space-x-3">
+                                                <div class="relative">
+                                                    <template x-if="authorMetaData[r.pubkey]">
+                                                        <img class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-amber-500"
+                                                             :src="authorMetaData[r.pubkey].image"
+                                                             :alt="authorMetaData[r.pubkey].display_name && authorMetaData[r.pubkey].display_name[0]"
+                                                        >
+                                                    </template>
+                                                    <span class="absolute -bottom-0.5 -right-1 rounded-tl bg-transparent px-0.5 py-px">
                                                   <svg class="h-5 w-5 text-purple-500" viewBox="0 0 20 20"
                                                        fill="currentColor"
                                                        aria-hidden="true">
@@ -66,42 +68,42 @@
                                                           clip-rule="evenodd"/>
                                                   </svg>
                                                 </span>
-                                            </div>
-                                            <div class="min-w-0 flex-1">
-                                                <template x-if="authorMetaData[r.pubkey]">
-                                                    <div>
-                                                        <div class="text-sm">
-                                                            <div class="font-bold text-gray-200"
-                                                                 x-text="authorMetaData[r.pubkey].display_name"></div>
+                                                </div>
+                                                <div class="min-w-0 flex-1">
+                                                    <template x-if="authorMetaData[r.pubkey]">
+                                                        <div>
+                                                            <div class="text-sm">
+                                                                <div class="font-bold text-gray-200"
+                                                                     x-text="authorMetaData[r.pubkey].display_name"></div>
+                                                            </div>
+                                                            <p class="mt-0.5 text-sm text-gray-300 italic"
+                                                               x-text="formatDate(r.created_at)"></p>
                                                         </div>
-                                                        <p class="mt-0.5 text-sm text-gray-300 italic"
-                                                           x-text="formatDate(r.created_at)"></p>
+                                                    </template>
+                                                    <div class="mt-2 text-sm text-gray-200">
+                                                        <p x-html="parseContent(r)"></p>
                                                     </div>
-                                                </template>
-                                                <div class="mt-2 text-sm text-gray-200">
-                                                    <p x-html="parseContent(r)"></p>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <template x-if="eventsReplies[r.id]">
-                                        <ul role="list" class="ml-6 mb-3">
-                                            <template x-for="(r2, i2) in eventsReplies[r.id]">
-                                                <li>
-                                                    <div class="relative pb-8">
+                                        <template x-if="eventsReplies[r.id]">
+                                            <ul role="list" class="ml-6 mb-3">
+                                                <template x-for="(r2, i2) in eventsReplies[r.id]">
+                                                    <li>
+                                                        <div class="relative pb-8">
                                                         <span
                                                                 x-show="i2 + 1 !== eventsReplies[r.id].length"
                                                                 class="absolute left-5 top-5 rotate-180 -ml-px h-full w-0.5 bg-amber-500"
                                                                 aria-hidden="true"></span>
-                                                        <div class="relative flex items-start space-x-3">
-                                                            <div class="relative">
-                                                                <template x-if="authorMetaData[r2.pubkey]">
-                                                                    <img class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-amber-500"
-                                                                         :src="authorMetaData[r2.pubkey].image"
-                                                                         :alt="authorMetaData[r2.pubkey].display_name && authorMetaData[r2.pubkey].display_name[0]"
-                                                                    >
-                                                                </template>
-                                                                <span class="absolute -bottom-0.5 -right-1 rounded-tl bg-transparent px-0.5 py-px">
+                                                            <div class="relative flex items-start space-x-3">
+                                                                <div class="relative">
+                                                                    <template x-if="authorMetaData[r2.pubkey]">
+                                                                        <img class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-amber-500"
+                                                                             :src="authorMetaData[r2.pubkey].image"
+                                                                             :alt="authorMetaData[r2.pubkey].display_name && authorMetaData[r2.pubkey].display_name[0]"
+                                                                        >
+                                                                    </template>
+                                                                    <span class="absolute -bottom-0.5 -right-1 rounded-tl bg-transparent px-0.5 py-px">
                                                                   <svg class="h-5 w-5 text-purple-500" viewBox="0 0 20 20"
                                                                        fill="currentColor"
                                                                        aria-hidden="true">
@@ -110,39 +112,40 @@
                                                                           clip-rule="evenodd"/>
                                                                   </svg>
                                                                 </span>
-                                                            </div>
-                                                            <div class="min-w-0 flex-1">
-                                                                <template x-if="authorMetaData[r2.pubkey]">
-                                                                    <div>
-                                                                        <div class="text-sm">
-                                                                            <div class="font-bold text-gray-200"
-                                                                                 x-text="authorMetaData[r2.pubkey].display_name"></div>
+                                                                </div>
+                                                                <div class="min-w-0 flex-1">
+                                                                    <template x-if="authorMetaData[r2.pubkey]">
+                                                                        <div>
+                                                                            <div class="text-sm">
+                                                                                <div class="font-bold text-gray-200"
+                                                                                     x-text="authorMetaData[r2.pubkey].display_name"></div>
+                                                                            </div>
+                                                                            <p class="mt-0.5 text-sm text-gray-300 italic"
+                                                                               x-text="formatDate(r2.created_at)"></p>
                                                                         </div>
-                                                                        <p class="mt-0.5 text-sm text-gray-300 italic"
-                                                                           x-text="formatDate(r2.created_at)"></p>
+                                                                    </template>
+                                                                    <div class="mt-2 text-sm text-gray-200">
+                                                                        <p x-html="parseContent(r2)"></p>
                                                                     </div>
-                                                                </template>
-                                                                <div class="mt-2 text-sm text-gray-200">
-                                                                    <p x-html="parseContent(r2)"></p>
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                </li>
-                                            </template>
-                                        </ul>
-                                    </template>
-                                </li>
-                            </template>
-                        </ul>
-                    </template>
-                </li>
-            </template>
-        </ul>
-    </div>
-</template>
-<template x-if="!eventsReplies[event.id]">
-    <div>
-        keine Antworten vorhanden
-    </div>
-</template>
+                                                    </li>
+                                                </template>
+                                            </ul>
+                                        </template>
+                                    </li>
+                                </template>
+                            </ul>
+                        </template>
+                    </li>
+                </template>
+            </ul>
+        </div>
+    </template>
+    <template x-if="!eventsReplies[event.id]">
+        <div>
+            keine Antworten vorhanden
+        </div>
+    </template>
+</div>

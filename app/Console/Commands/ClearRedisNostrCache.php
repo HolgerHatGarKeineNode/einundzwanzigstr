54406f63e4d7610e50ec8366295fbc9897a09d18
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-08-31 16:35:30 +0200

    updates

---
+++ app/Console/Commands/ClearRedisNostrCache.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class ClearRedisNostrCache extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clearnostr';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $redis = Redis::connection('nostr')->client();
+        $redis->del('events');
+        $redis->del('npubs');
+    }
+}

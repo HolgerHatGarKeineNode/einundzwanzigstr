9f02584fe1b1abb2429071f10e0261e04b231338
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-09-07 13:47:31 +0200

    livewire components refactor

--- app/Livewire/Nostr/Replies.php
+++ app/Livewire/Nostr/Replies.php
@@ -2,11 +2,14 @@
 
 namespace App\Livewire\Nostr;
 
+use App\Traits\RenderHtml;
 use Illuminate\Support\Facades\Redis;
 use Livewire\Component;
 
 class Replies extends Component
 {
+    use RenderHtml;
+
     public $event;
     public array $replies = [];
 
@@ -20,14 +23,29 @@ class Replies extends Component
         $redisClient = Redis::connection('nostr')->client();
         $replies = $redisClient->hGet('replies', $this->event['id']);
         if ($replies) {
-            $this->replies = json_decode($replies, true, 512, JSON_THROW_ON_ERROR);
+            $this->replies = collect(json_decode($replies, true, 512, JSON_THROW_ON_ERROR))
+                ->map($this->renderReplyHtml())->toArray();
         }
+    }
+
+    public function renderReplyHtml(): \Closure
+    {
+        return function ($reply) {
+            $reply['renderedHtml'] = $this->renderHtml($reply['content'], $reply['id']);
+            if (count($reply['children']) > 0) {
+                $reply['children'] = collect($reply['children'])
+                    ->map($this->renderReplyHtml())
+                    ->toArray();
+            }
+            return $reply;
+        };
     }
 
     public function cacheReplies($replies)
     {
         $redisClient = Redis::connection('nostr')->client();
-        $redisClient->hSet('replies', $this->event['id'], json_encode($replies, JSON_THROW_ON_ERROR));
+        $redisClient->hSet('replies', $this->event['id'], json_encode(collect($replies)
+            ->map($this->renderReplyHtml())->toArray(), JSON_THROW_ON_ERROR));
     }
 
     public function render()
